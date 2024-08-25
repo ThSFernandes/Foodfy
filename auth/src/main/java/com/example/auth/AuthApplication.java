a64b963a10b148c301df46362841fb000de1fe9a
Luiz Filipe <luizferamos26@gmail.com>
2024-08-24 23:45:04 -0300

    Auth Server integrado corretamente com o Config Server. Retirado dependência do FlyWay. Criado os endpoints /checker/env e /checker/db-test para checagens.

--- auth/src/main/java/com/example/auth/AuthApplication.java
+++ auth/src/main/java/com/example/auth/AuthApplication.java
@@ -3,15 +3,14 @@ package com.example.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.auth.repositories")
 @EntityScan(basePackages = "com.example.auth.domain")
+
 public class AuthApplication {
 
 	public static void main(String[] args) {
