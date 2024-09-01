868b87a9e198fe808c6bc406a384aa30edbb805c
Luiz-Filipe-26 <luizferamos26@gmail.com>
2024-08-31 22:34:20 -0300

    Casca do projeto do stock e configurações feitas para ele.

--- auth/src/main/java/com/example/auth/infra/security/SecurityFilter.java
+++ auth/src/main/java/com/example/auth/infra/security/SecurityFilter.java
@@ -25,7 +25,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
         if (token != null) {
-            var login = tokenService.validateToken(token);
+            String login = tokenService.validateToken(token);
             UserDetails user = userRepository.findByLogin(login);
 
             if (user != null) {
