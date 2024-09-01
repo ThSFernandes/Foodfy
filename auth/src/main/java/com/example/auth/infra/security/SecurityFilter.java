de46c8b72705cd92995212f70cf7ecefd5aaecde
Luiz-Filipe-26 <luizferamos26@gmail.com>
2024-08-31 21:24:09 -0300

    Pequenas mudanças

--- auth/src/main/java/com/example/auth/infra/security/SecurityFilter.java
+++ auth/src/main/java/com/example/auth/infra/security/SecurityFilter.java
@@ -20,7 +20,8 @@ public class SecurityFilter extends OncePerRequestFilter {
     TokenService tokenService;
     @Autowired
     UserRepository userRepository;
-
+    
+    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
         if (token != null) {
